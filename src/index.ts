269bc3951ebce892f5c6b17b630a1cfb62611c89
jintekc <info@jintek.consulting>
2025-03-06 20:45:19 -0500

    2.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-export * from './keys/interface.js';
-export * from './keys/key-pair.js';
-export * from './keys/private-key.js';
-export * from './keys/public-key.js';
-
+export * from './di-bip340/cryptosuite/canonicalize.js';
 export * from './di-bip340/cryptosuite/index.js';
 export * from './di-bip340/cryptosuite/interface.js';
 
@@ -12,9 +8,16 @@ export * from './di-bip340/data-integrity-proof/interface.js';
 export * from './di-bip340/multikey/index.js';
 export * from './di-bip340/multikey/interface.js';
 
+export * from './keys/constants.js';
+export * from './keys/interface.js';
+export * from './keys/key-pair.js';
+export * from './keys/private-key.js';
+export * from './keys/public-key.js';
+
 export * from './types/cryptosuite.js';
 export * from './types/di-proof.js';
 export * from './types/shared.js';
 
 export * from './utils/error.js';
+export * from './utils/logger.js';
 export * from './utils/object-utils.js';
